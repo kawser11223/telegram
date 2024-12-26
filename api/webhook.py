3bde131bca4939650bb7481bfcccd8bca9378dad
kawser11223 <148429350+kawser11223@users.noreply.github.com>
2024-12-26 21:36:08 +0600

    test

---
+++ api/webhook.py
@@ -0,0 +1,10 @@
+import requests
+
+BOT_TOKEN = "7947742121:AAEyNzPDyfS-TE9Uq1lesFScsC-nahaKIZI"
+WEBHOOK_URL = "https://https://telegram-jf1m.vercel.app/api/webhook"
+
+response = requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook",
+    json={"url": WEBHOOK_URL},
+)
+print(response.json())
