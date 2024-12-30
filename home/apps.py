3768730af6d00ce168dd1cc9852cf8fcc66c705d
kawser11223 <148429350+kawser11223@users.noreply.github.com>
2024-12-30 11:59:18 +0600

    'test

--- home/apps.py
+++ home/apps.py
@@ -1,10 +1,7 @@
-
 from django.apps import AppConfig
-from .views import start_bot_in_thread
+
 
 class MyAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
 
-    def ready(self):
-        start_bot_in_thread()
